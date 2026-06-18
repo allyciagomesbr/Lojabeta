fd45e47a1931642ea689b9bc7436e4fef87df21c
Allycia Gomes <allyciagomesribeiro@gmail.com>
2026-06-18 16:26:24 -0300

    Update bd.js

--- bd.js
+++ bd.js
@@ -1,8 +1,12 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './bd.sqlite'
-});
-
-module.exports = sequelize;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/roupas">Roupas</a>
+    
+</body>
+</html>
